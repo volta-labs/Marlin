07652ebf2df6e827908950b401698677b75ccc0f
James Ferguson <wjcferguson@gmail.com>
2021-10-25 10:45:50 -0400

    Turn on AUTO_REPORT_POSITION and M114_REALTIME
    This allows the firmware to asynchronously report its position, and allows us to
    ask for the real position.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3553,7 +3553,7 @@
 /**
  * Auto-report position with M154 S<seconds>
  */
-//#define AUTO_REPORT_POSITION
+#define AUTO_REPORT_POSITION
 
 /**
  * Include capabilities in M115 output
@@ -3609,7 +3609,7 @@
 
 // Extra options for the M114 "Current Position" report
 //#define M114_DETAIL         // Use 'M114` for details to check planner calculations
-//#define M114_REALTIME       // Real current position based on forward kinematics
+#define M114_REALTIME       // Real current position based on forward kinematics
 //#define M114_LEGACY         // M114 used to synchronize on every call. Enable if needed.
 
 //#define REPORT_FAN_CHANGE   // Report the new fan speed when changed by M106 (and others)
