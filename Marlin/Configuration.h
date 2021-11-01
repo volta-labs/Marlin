ed67966100b8adb3d7a6fc365656145ac4bf7bfc
James Ferguson <wjcferguson@gmail.com>
2021-11-01 17:40:25 -0400

    Adjust homing speeds
    Faster Z homing, but slower bumps all round

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1715,7 +1715,7 @@
 #endif
 
 // Homing speeds (mm/min)
-#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (4*60) }
+#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (50*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
